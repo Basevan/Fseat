bc867e2bb68a38eb4f48107bae8b46978584db71
basevan <183320926@qq.com>
2019-08-01 16:08:44 +0800

    update 'pageController'

--- src/main/java/com/van/controller/pageController.java
+++ src/main/java/com/van/controller/pageController.java
@@ -1,14 +1,18 @@
 package com.van.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.van.model.User;
 import com.van.utils.BaseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
@@ -30,10 +34,26 @@ public class pageController {
         return "hello world!";
     }
 
-    @RequestMapping("/login")
+    @RequestMapping("/getUser")
     public JSONObject Login() {
         List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from student");
         return BaseResult.success(list);
+    }
+
+    @RequestMapping("/login")
+    public JSONObject login(@Valid User user, BindingResult bindingResult) {
+        StringBuffer stringBuffer = new StringBuffer();
+        if(bindingResult.hasErrors()){
+            List<ObjectError> list =bindingResult.getAllErrors();
+            for (ObjectError objectError:list) {
+                stringBuffer.append(objectError.getDefaultMessage());
+                stringBuffer.append("---");
+            }
+            return BaseResult.fail(stringBuffer.toString());
+        }
+        else {
+            return BaseResult.success();
+        }
     }
 
     @RequestMapping("/logout")
